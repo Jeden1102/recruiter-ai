fc7d2a4d12932f1dbd36d0a04c61b3319b6ac959
Jeden1102 <bum123@o2.pl>
2024-12-10 22:22:19 +0100

    init commit

---
+++ components/Chat.vue
@@ -0,0 +1,50 @@
+<template>
+  <Card class="pt-20">
+    <CardContent class="flex flex-col gap-4">
+      <Textarea v-model="inputData" />
+      <Button @click="sendMessage" class="w-fit"> Send message </Button>
+      <div>
+        <div v-for="chat in chatTree" :key="chat">
+          <template v-if="chat.role !== 'system'">
+            <strong>{{ chat.role }} :</strong>
+            <div>{{ chat.content }}</div>
+          </template>
+        </div>
+      </div>
+    </CardContent>
+  </Card>
+</template>
+<script setup lang="ts">
+const { chatCompletion } = useChatgpt();
+
+const chatTree = ref([
+  {
+    role: "system",
+    content:
+      "Jesteś asystentem AI, który generuje przykładowe pytania i odpowiedzi na rozmowę o pracę w formacie JSON. Użytkownik poda listę wymagań, a Twoim zadaniem jest zwrócenie odpowiednich danych w postaci struktury JSON.",
+  },
+]);
+const inputData = ref("");
+
+async function sendMessage() {
+  try {
+    const message = {
+      role: "user",
+      content: `${inputData.value}`,
+    };
+
+    chatTree.value.push(message);
+
+    const response = await chatCompletion(chatTree.value);
+
+    const responseMessage = {
+      role: response[0].message.role,
+      content: response[0].message.content,
+    };
+
+    chatTree.value.push(responseMessage);
+  } catch (error) {
+    alert(`Join the waiting list if you want to use GPT-4 models: ${error}`);
+  }
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+  modules: ["nuxt-chatgpt", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  chatgpt: {
+    apiKey: process.env.OPENAI_API_KEY,
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
