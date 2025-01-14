153215139b14d371ff153e5b53313dfd5b70a2a0
Jeden1102 <43098219+Jeden1102@users.noreply.github.com>
2025-01-14 08:27:58 +0100

    master. AboveFooter added auto margin top.

--- components/app/AboveFooter.vue
+++ components/app/AboveFooter.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="mt-8">
-    <div
-      class="container relative mt-auto flex flex-col items-center justify-center gap-4 overflow-hidden py-8 lg:py-12"
-    >
-      <p class="flex flex-col items-center text-2xl lg:text-5xl">
-        <span class="mb-2 lg:mb-4">{{
-          $t("home.callToAction.startWith")
-        }}</span>
-        <TextGradient>
-          <span>{{ $t("home.callToAction.poweredTool") }}</span>
-        </TextGradient>
-      </p>
+  <div class="mt-auto">
+    <div class="mt-8">
+      <div
+        class="container relative mt-auto flex flex-col items-center justify-center gap-4 overflow-hidden py-8 lg:py-12"
+      >
+        <p class="flex flex-col items-center text-2xl lg:text-5xl">
+          <span class="mb-2 lg:mb-4">{{
+            $t("home.callToAction.startWith")
+          }}</span>
+          <TextGradient>
+            <span>{{ $t("home.callToAction.poweredTool") }}</span>
+          </TextGradient>
+        </p>
 
-      <p class="text-center text-3xl font-light">
-        {{ $t("home.callToAction.assistantDescription") }}
-      </p>
-      <Button asChild size="lg" class="mt-8">
-        <NuxtLinkLocale to="/recruiter">{{
-          $t("common.getStarted")
-        }}</NuxtLinkLocale>
-      </Button>
-      <NuxtImg
-        src="/images/decorative.svg"
-        width="580"
-        height="520"
-        :alt="$t('home.callToAction.decorativeImageAlt')"
-        class="absolute -bottom-3/4 left-1/2 -z-10 -translate-x-1/2 opacity-20"
-      />
+        <p class="text-center text-3xl font-light">
+          {{ $t("home.callToAction.assistantDescription") }}
+        </p>
+        <Button asChild size="lg" class="mt-8">
+          <NuxtLinkLocale to="/recruiter">{{
+            $t("common.getStarted")
+          }}</NuxtLinkLocale>
+        </Button>
+        <NuxtImg
+          src="/images/decorative.svg"
+          width="580"
+          height="520"
+          :alt="$t('home.callToAction.decorativeImageAlt')"
+          class="absolute -bottom-3/4 left-1/2 -z-10 -translate-x-1/2 opacity-20"
+        />
+      </div>
     </div>
   </div>
 </template>
