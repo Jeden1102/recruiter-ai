0a254cbb072170817e859a0dd59340f364ab7dd3
Jeden1102 <bum123@o2.pl>
2025-02-11 18:47:33 +0100

    fix: different template fetch try

--- server/api/mail.ts
+++ server/api/mail.ts
@@ -1,13 +1,25 @@
-export default defineEventHandler(async () => {
-  try {
-    const { sendMail } = useNodeMailer();
+import Handlebars from "handlebars";
 
-    return sendMail({
-      subject: "Account confirmation",
-      to: "dom.rad14@gmail.com",
-      text: "test mail",
-    });
-  } catch (error) {
-    return error;
+export default defineEventHandler(async () => {
+  const { sendMail } = useNodeMailer();
+
+  const source = await useStorage().getItem(
+    "root:assets:mail-templates/account-confirmation.html",
+  );
+
+  if (!source) {
+    throw new Error("Email template not found!");
   }
+
+  const template = Handlebars.compile(source);
+  const htmlContent = template({
+    name: "Dominik",
+    url: "http://localhost:3000/account/confirm?code=1234",
+  });
+
+  return sendMail({
+    subject: "Account confirmation",
+    to: "dom.rad14@gmail.com",
+    html: htmlContent,
+  });
 });
